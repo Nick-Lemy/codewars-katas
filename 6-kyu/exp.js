e06e44d82a46e2e2a9bea6861b205a619a895bb4
Nick-Lemy <n.kayiranga@alustudent.com>
2024-11-11 15:54:15 +0200

    Add example of using for...of loop for array iteration

---
+++ 6-kyu/exp.js
@@ -0,0 +1,8 @@
+
+// No need for doing for loop with the length of the list
+var names = ['Nick', 'John', 'Paul', 'George', 'Ringo'];
+//Here is a better way to interate over the list
+for (const name of names) {
+  console.log(name);
+}
+//but still you couldn't use the index of the element :sad:
