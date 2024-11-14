191bdc8bc84f946206372ee7a63d1b32f5b87936
Nick-Lemy <n.kayiranga@alustudent.com>
2024-11-14 16:03:20 +0200

    Damn this is too much

--- 6-kyu/Array.diff.js
+++ 6-kyu/Array.diff.js
@@ -1,21 +1,35 @@
-// Online Javascript Editor for free
-// Write, Edit and Run your Javascript code using JS Online Compiler
+// // Online Javascript Editor for free
+// // Write, Edit and Run your Javascript code using JS Online Compiler
+
+// function arrayDiff(a, b) {
+//     let result = [];
+//     for (let i = 0; i < a.length; i++) {
+//           for (let j = 0; j < b.length; j++) {
+//             if (a[i] !== b[j]) {
+//                 result.push(a[i])
+//                 console.log(b);
+//             } else {
+//                 a.splice(i, 1)
+//             }
+//         }
+//     }
+//     return a.length === 0 || b.length === 0  ? a : result;
+// }
+
 
 function arrayDiff(a, b) {
     let result = [];
-    for (let i = 0; i < a.length; i++) {
-          for (let j = 0; j < b.length; j++) {
-            if (a[i] !== b[j]) {
-                result.push(a[i])
-                console.log(b);
-            } else {
-                
+    for (let i = 0; i < b.length; i++) {
+        for (let j = 0; j < a.length; j++) {
+            if (b[i] === a[j]) {
+                a.splice(j, 1);
             }
         }
     }
-    return a.length === 0 || b.length === 0  ? a : result;
+    return a;
 }
-// console.log(arrayDiff([1,2], [1]));
-// console.log(arrayDiff([1,2,2], [1]));
-// console.log(arrayDiff([1,2,2], [2]));
-console.log(arrayDiff([1,2,3], [1,2]));
+
+console.log(arrayDiff([1, 2, 3], [1, 2]));
+console.log(arrayDiff([1, 2], [1]));
+console.log(arrayDiff([1, 2, 2], [1]));
+console.log(arrayDiff([1, 2, 2], [2]));
