c6f3c99b4ffa81eebb222aa37efc720f6f72bd75
Nick-Lemy <n.kayiranga@alustudent.com>
2024-11-18 17:39:04 +0200

    split string kata

---
+++ 6-kyu/Split_Strings.js
@@ -0,0 +1,10 @@
+function solution(str){
+    let result = [];
+     if(str.length % 2 != 0){
+       str += '_'
+     }
+     for(let i = 0; i < str.length; i += 2){
+      result.push(str[i]+str[i+1]);
+     }
+    return result;
+  }
