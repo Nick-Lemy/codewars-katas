864caef237b7ad7bf4d95eb72d7063e3b1b16d50
Nick-Lemy <n.kayiranga@alustudent.com>
2024-11-18 17:48:19 +0200

    reformated

--- 7-kyu/Basic_Sequence_Practice.js
+++ 7-kyu/Basic_Sequence_Practice.js
@@ -1,8 +1,7 @@
 function sumOfN(n) {
     let result = [0];
-    for(let i = 1; i <= Math.abs(n); i++){
-      result.push(n >= 0 ? (result[i - 1] + i) : - (- result[i - 1] + i));
+    for (let i = 1; i <= Math.abs(n); i++) {
+        result.push(n >= 0 ? (result[i - 1] + i) : - (- result[i - 1] + i));
     }
     return result;
-  }
-  
+}
