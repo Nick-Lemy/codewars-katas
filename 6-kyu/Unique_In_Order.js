df3be9874e5ba41348e24a39117dbbe483d97271
Nick-Lemy <n.kayiranga@alustudent.com>
2024-11-18 17:52:42 +0200

    ok enough !

--- 6-kyu/Unique_In_Order.js
+++ 6-kyu/Unique_In_Order.js
@@ -1,12 +1,12 @@
 // Here is the kata's link https://www.codewars.com/kata/54e6533c92449cc251001667/javascript
 // Have a fun! :D
 
-var uniqueInOrder=function(iterable){
+var uniqueInOrder = function (iterable) {
   let result = [iterable[0]];
-  for(let i = 0; i < iterable.length; i++){
-    if(result[result.length - 1] !== iterable[i]){
+  for (let i = 0; i < iterable.length; i++) {
+    if (result[result.length - 1] !== iterable[i]) {
       result.push(iterable[i]);
     }
   }
-  return iterable.length == 0 ? [] : result ;
+  return iterable.length == 0 ? [] : result;
 }
