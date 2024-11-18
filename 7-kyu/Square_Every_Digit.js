ce42b47bae5ebce91c1b3db6f0fcb5088c2f0820
Nick-Lemy <n.kayiranga@alustudent.com>
2024-11-18 17:49:59 +0200

    On file has been reformatted

--- 7-kyu/Square_Every_Digit.js
+++ 7-kyu/Square_Every_Digit.js
@@ -2,7 +2,7 @@ function squareDigits(num) {
     let numero = num.toString();
     let result = "";
     for (let i = 0; i < numero.length; i++) {
-        result += (parseInt(numero[i])**2).toString();
+        result += (parseInt(numero[i]) ** 2).toString();
     }
     return parseInt(result);
 }
