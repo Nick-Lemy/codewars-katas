4c5517b5fd963f22da5618c443687a291a029d12
Nick-Lemy <n.kayiranga@alustudent.com>
2024-11-19 16:22:08 +0200

    Well, I just factorized it broo. this is amazing

--- 7-kyu/Basic_Sequence_Practice.js
+++ 7-kyu/Basic_Sequence_Practice.js
@@ -1,7 +1,1 @@
-function sumOfN(n) {
-    let result = [0];
-    for (let i = 1; i <= Math.abs(n); i++) {
-        result.push(n >= 0 ? (result[i - 1] + i) : - (- result[i - 1] + i));
-    }
-    return result;
-}
+let makeString = s => s.split(' ').map(x => x[0]).join('')
