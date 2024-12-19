801daf26c98ad6397de5e8a56233c5893e9cc231
Nick-Lemy <n.kayiranga@alustudent.com>
2024-12-19 17:13:56 +0200

    new 6 kyu

--- 6-kyu/Find_the_odd_int.js
+++ 6-kyu/Find_the_odd_int.js
@@ -1,22 +1,8 @@
 function findOdd(A) {
-    let result = [A[0]];
-    let sorted_list = A.sort();
-    for (let i = 1; i < sorted_list.length; i++) {
-        console.log(result);
-        
-        console.log(sorted_list[i], sorted_list[i - 1]);
-        if (sorted_list[i - 1] === sorted_list[i]) {
-            result.push(sorted_list[i]);
-        } else {
-            if (result.length % 3 === 0 || A.length === 1) {
-                return result[0];
-            } 
-            else {
-                result = [sorted_list[i]];
-            }
-        }
-    }
-    return result[0];
+  const map = {};
+  A.forEach((elem) => {
+    map[elem] = (map[elem] || 0) + 1;
+  });
+  let r = Object.entries(map);
+  return Number(r.filter((n) => n[1] % 2 !== 0)[0][0]);
 }
-
-console.log(findOdd([1,1,2,2,2,2,3,3,3,3,3,3,4])) // 3;
